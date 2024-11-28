5f1f249ea4359e3c5df56480e40fe9ec80e37ab0
ZeManS150187 <zemansleon@gmail.com>
2024-11-28 19:49:09 +0300

    Lesson6

--- src/main/java/Lesson6/Main.java
+++ src/main/java/Lesson6/Main.java
@@ -1,9 +1,6 @@
 package Lesson6;
 
 
-import hw_aqa6.Employee;
-import hw_aqa6.Park;
-
 public class Main {
     public static void main(String[] args) {
         //Задание 2
@@ -11,14 +8,14 @@ public class Main {
           Конструктор класса должен заполнять эти поля при создании объекта.
           Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.*/
 
-        hw_aqa6.Employee[] employees = new hw_aqa6.Employee[5];
+        Employee[] employees = new Employee[5];
 
 
-        employees[0] = new hw_aqa6.Employee("Ivanov Ivan", "Business Analyst", "ivanov@mail.com", "1234567890", 60000, 30);
-        employees[1] = new hw_aqa6.Employee("Petrova Anna", "Java developer", "anna@mail.com", "2345678901", 80000, 28);
-        employees[2] = new hw_aqa6.Employee("Sidorov Alexey", "QA", "alexey@mail.com", "3456789012", 50000, 35);
-        employees[3] = new hw_aqa6.Employee("Smirnov Sergey", "HR Specialist", "sergey@mail.com", "4567890123", 45000, 40);
-        employees[4] = new hw_aqa6.Employee("Kuznetsova Olga", "AQA", "olga@mail.com", "5678901234", 58000, 32);
+        employees[0] = new Employee("Ivanov Ivan", "Business Analyst", "ivanov@mail.com", "1234567890", 60000, 30);
+        employees[1] = new Employee("Petrova Anna", "Java developer", "anna@mail.com", "2345678901", 80000, 28);
+        employees[2] = new Employee("Sidorov Alexey", "QA", "alexey@mail.com", "3456789012", 50000, 35);
+        employees[3] = new Employee("Smirnov Sergey", "HR Specialist", "sergey@mail.com", "4567890123", 45000, 40);
+        employees[4] = new Employee("Kuznetsova Olga", "AQA", "olga@mail.com", "5678901234", 58000, 32);
 
         // Выводим в консоль информацию о сотрудниках
         for (Employee employee : employees) {
@@ -28,10 +25,9 @@ public class Main {
 
         //Задание 3.
         //Так, для проверки
-
-        hw_aqa6.Park.Attraction attraction = new hw_aqa6.Park.Attraction("Вальс", "С 9.00 - 18.00", 5);
-        hw_aqa6.Park.Attraction attraction1 = new hw_aqa6.Park.Attraction("Ромашка", "С 9.00 - 18.00", 4);
-        hw_aqa6.Park.Attraction attraction2 = new Park.Attraction("Сюрприз", "С 9.00 - 18.00", 6);
+        Park.Attraction attraction = new Park.Attraction("Вальс", "С 9.00 - 18.00", 5);
+        Park.Attraction attraction1 = new Park.Attraction("Ромашка", "С 9.00 - 18.00", 4);
+        Park.Attraction attraction2 = new Park.Attraction("Сюрприз", "С 9.00 - 18.00", 6);
 
         attraction.displayAttractionInfo();
         System.out.println();
