c8a6323d37afeb639454dc0cde7a048ecd70405e
ZeManS150187 <zemansleon@gmail.com>
2024-12-06 18:11:30 +0300

    Lesson10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,5 +45,21 @@ public class Main {
         System.out.println("Количество котов, которые поели: " + fedCats);
         System.out.println("Количество голодных котов: " + hungryCats);
         System.out.println("В миске осталось " + bowl.getFood() + " еды.");
+
+        //Задание 2
+        // Создаем фигуры
+        Shape circle = new Circle(5); // Круг с радиусом 5
+        Shape rectangle = new Rectangle(4, 7); // Прямоугольник 4x7
+        Shape triangle = new Triangle(3, 4, 5); // Треугольник со сторонами 3, 4, 5
+
+        // Задаем цвета для каждой фигуры и выводим характеристики
+        System.out.println("Круг:");
+        circle.printCharacteristics("Красный", "Черный");
+
+        System.out.println("\nПрямоугольник:");
+        rectangle.printCharacteristics("Синий", "Зеленый");
+
+        System.out.println("\nТреугольник:");
+        triangle.printCharacteristics("Желтый", "Фиолетовый");
     }
 }
